033ff7f51f09b39df25ad475d69b7a406c4df1b1
Paty-star <80486451+Paty-star@users.noreply.github.com>
2021-06-01 16:08:46 -0300

    componente inicio sem consumo

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs'
 import { User } from '../model/User';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -23,6 +24,17 @@ export class AuthService {
     return this.http.post<User>('http://localhost8080/usuarios/cadastrar',user)
 
   }
+  logado(){
+    let ok: boolean =false
 
+    if (environment.token !=''){
+      ok= true
+    }
+    
+
+    
+
+    return ok
+  }
 
 }

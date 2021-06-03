bd33e9948db23feb62b2c8477a42fb028fe516f0
Paty-star <80486451+Paty-star@users.noreply.github.com>
2021-06-03 11:48:08 -0300

    implementação do get-post-tema

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -10,8 +12,13 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TemaComponent implements OnInit {
 
+  tema:Tema = new Tema ()
+  listaTemas: Tema[]
+    
+
   constructor(
-    private router: Router
+    private router: Router,
+    private temaService: TemaService
   ) { }
 
   ngOnInit() {
@@ -19,6 +26,24 @@ export class TemaComponent implements OnInit {
     if(environment.token==''){
       this.router.navigate(['/entrar'])
     }
+
+    this.findAllTemas()
+  } 
+
+  findAllTemas(){
+    this.temaService.getAllTema().subscribe((resp:Tema[])=>{
+      this.listaTemas = resp 
+    })
+  }
+
+
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe((resp:Tema)=>{
+      this.tema = resp
+      alert('Tema cadastrado com sucesso!')
+      this.findAllTemas()
+      this.tema = new Tema()
+    })
   }
 
 }
